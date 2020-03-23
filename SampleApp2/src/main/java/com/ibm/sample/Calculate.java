d093a608467caee4e7c2d1a58b45a3e1c83f9e75
MANISH ANEJA <mananeja@in.ibm.com>
2020-03-23 15:36:09 +0530

    Initial Commit

---
+++ SampleApp2/src/main/java/com/ibm/sample/Calculate.java
@@ -0,0 +1,21 @@
+package com.ibm.sample;
+
+public class Calculate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      int num1 = 100;
+	      int num2 = 20;
+
+	      System.out.println("num1 + num2: " + (num1 + num2) );
+	      System.out.println("num1 - num2: " + (num1 - num2) );
+	      System.out.println("num1 * num2: " + (num1 * num2) ); 
+	      System.out.println("num1 / num2: " + (num1 / num2) );
+	      System.out.println("num1 % num2: " + (num1 % num2) );
+	   }
+	
+	public static int cube(int n){  
+		int num = 20;
+        return num*num*num; 
+	}
+}
